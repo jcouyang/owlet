91d24d51f67cf2df41a38097e986e160d4cddc9d
Jichao Ouyang <oyanglulu@gmail.com>
2018-05-05 23:05:07 +1000

    rank2

--- src/main/scala/owlet/Owlet.scala
+++ src/main/scala/owlet/Owlet.scala
@@ -1,19 +1,31 @@
 package owlet
 
+import cats.Functor
+import cats.data.Reader
 import outwatch.dom.{  _ }
 import outwatch.dom.dsl._
 import monix.execution.Scheduler.Implicits.global
 import outwatch.Handler._
-// import cats.syntax.apply._
-object Owlet {
+import cats.syntax.functor._
+import cats.{~>, Id}
+object Main {
 
-  case class OComponent[A](handler: Handler[A], node:VNode)
+  case class Owlet[A](node:Id ~> ({type RR[C] = Reader[Observable[C], VNode]})#RR, signal: Observable[A])
+
+  implicit val functorOwlet = new Functor[Owlet] {
+    def map[A,B](fa: Owlet[A])(f: A=>B) = {
+      Owlet(fa.node, fa.signal.map(f))
+    }
+  }
+
+  def oinput(n: String) = create[String].map(handler =>
+    Owlet(new (Id ~> ({type RR[C] = Reader[Observable[C], VNode]})#RR) {
+      def apply[A](a:A):Reader[Observable[A], VNode] = Reader((signal:Observable[A]) => input(tpe := "text", name := n, onInput.value --> handler))
+    }, handler)
+    )
 
   def main(args: scala.Array[String]): Unit = {
-    val source = create[String]
-    val inputE = source.map(handler => OComponent(handler,input(tpe := "text", name := "hehe", onInput.value --> handler)))
-
-    val d = inputE.flatMap(i=> div(i.node, child <-- i.handler))
-    OutWatch.renderInto("#app", d) unsafeRunSync ()
+    // val output = oinput("hehe").map(o => o.map(_ + a))
+    // OutWatch.renderInto("#app", output.flatMap(_.node)) unsafeRunSync ()
   }
 }
