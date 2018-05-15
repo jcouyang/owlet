4c8e218f3f19260bdd98538747b7898751a55ed5
Jichao Ouyang <oyanglulu@gmail.com>
2018-05-15 17:49:37 +1000

    applicative owlet

--- src/main/scala/owlet/Owlet.scala
+++ src/main/scala/owlet/Owlet.scala
@@ -1,31 +1,52 @@
 package owlet
 
-import cats.Functor
-import cats.data.Reader
+import cats.Applicative
+import cats.effect.IO
+import cats.{ Functor }
 import outwatch.dom.{  _ }
 import outwatch.dom.dsl._
 import monix.execution.Scheduler.Implicits.global
 import outwatch.Handler._
 import cats.syntax.functor._
-import cats.{~>, Id}
+import cats.syntax.apply._
+import monix.reactive.Observable
 object Main {
 
-  case class Owlet[A](node:Id ~> ({type RR[C] = Reader[Observable[C], VNode]})#RR, signal: Observable[A])
-
+  case class Owlet[A](node:VNode, signal: Observable[A])
   implicit val functorOwlet = new Functor[Owlet] {
     def map[A,B](fa: Owlet[A])(f: A=>B) = {
       Owlet(fa.node, fa.signal.map(f))
     }
   }
 
-  def oinput(n: String) = create[String].map(handler =>
-    Owlet(new (Id ~> ({type RR[C] = Reader[Observable[C], VNode]})#RR) {
-      def apply[A](a:A):Reader[Observable[A], VNode] = Reader((signal:Observable[A]) => input(tpe := "text", name := n, onInput.value --> handler))
-    }, handler)
+  implicit val applicativeOwlet = new Applicative[Owlet] {
+    def ap[A, B](ff: Owlet[A => B])(fa: Owlet[A]): Owlet[B] = Owlet(fa.node, ff.signal.ap(fa.signal))
+    def pure[A](a: A) = Owlet(div(), Observable.pure[A](a))
+  }
+
+  case class OwletT[F[_], A](value: F[Owlet[A]]) {
+    def map[B](f:A=>B)(implicit F: Functor[F]): OwletT[F, B] = {
+      OwletT(F.map(value)(_.map(f)))
+    }
+  }
+
+  def string(n: String) = create[String].map(handler =>
+    Owlet(input(tpe := "text", name := n, onInput.value --> handler),handler)
     )
 
+  def int(n: String) = create[Int].map(handler =>
+    Owlet(input(tpe := "number", name := n, onInput.value.map(v=>v.toDouble.toInt) --> handler), handler)
+  )
+
+  def number(n: String): IO[Owlet[Double]] = create[Double].map{ handler =>
+    val element = IO(onInput.value.map(v=>v.toDouble))
+      .flatMap(e => input(tpe := "number", name := n, e --> handler))
+    Owlet(element, handler)
+  }
+
+  def ooutput[A](o: Owlet[A]) = div(o.node, child <-- o.signal.map(_.toString))
+
   def main(args: scala.Array[String]): Unit = {
-    // val output = oinput("hehe").map(o => o.map(_ + a))
-    // OutWatch.renderInto("#app", output.flatMap(_.node)) unsafeRunSync ()
+    OutWatch.renderInto("#app", OwletT((number("hehe"), number("hoho")).mapN((a,b) =>a + b)).value.flatMap(ooutput)) unsafeRunSync ()
   }
 }
